1f3fc2cb6091c15894c44dd2462103c734099585
zhyyu <juror.yu@mart.blue>
2020-01-03 16:29:43 +0800

    DocApis

--- src/main/java/com/zhyyu/learn/es/LearnEsApplication.java
+++ src/main/java/com/zhyyu/learn/es/LearnEsApplication.java
@@ -1,3 +1,4 @@
+/*
 package com.zhyyu.learn.es;
 
 import org.springframework.boot.SpringApplication;
@@ -11,3 +12,4 @@ public class LearnEsApplication {
     }
 
 }
+*/
--- src/test/java/com/zhyyu/learn/es/LearnEsApplicationTests.java
+++ src/test/java/com/zhyyu/learn/es/LearnEsApplicationTests.java
@@ -1,3 +1,4 @@
+/*
 package com.zhyyu.learn.es;
 
 import org.junit.jupiter.api.Test;
@@ -11,3 +12,4 @@ class LearnEsApplicationTests {
     }
 
 }
+*/

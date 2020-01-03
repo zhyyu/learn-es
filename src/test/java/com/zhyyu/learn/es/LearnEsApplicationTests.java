b432ad9b321e07c074d5029d7fff7d366ac6cd9c
zhyyu <juror.yu@mart.blue>
2020-01-03 11:25:28 +0800

    init import

---
+++ src/test/java/com/zhyyu/learn/es/LearnEsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhyyu.learn.es;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnEsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
